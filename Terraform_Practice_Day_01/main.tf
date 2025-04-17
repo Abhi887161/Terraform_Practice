06a73f016a3980a4e47660be31e8943e3a95f0dc
abhi <abhishek98@gmail.com>
2025-04-17 12:00:06 +0530

    Day-02 push

--- Terraform_Practice_Day_01/main.tf
+++ Terraform_Practice_Day_01/main.tf
@@ -1,8 +1,8 @@
-#resource "aws_instance" "name" {
-#    ami = var.amiID
-#    instance_type = var.instance_type
-#    tags = {
-#        Name = "First_Server"
-#    }
+    resource "aws_instance" "name" {
+    ami = var.amiID
+    instance_type = var.instance_type
+    tags = {
+            Name = "First_Server"
+    }
   
-#}
+}
