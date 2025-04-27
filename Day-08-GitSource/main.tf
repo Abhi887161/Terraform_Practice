5699497b23fe74ce5f411ce6a2f721368ab09cda
abhi <abhishek98@gmail.com>
2025-04-27 18:05:52 +0530

    add files

---
+++ Day-08-GitSource/main.tf
@@ -0,0 +1,7 @@
+module "name" {
+    source = "github.com/Abhi887161/Terraform_Practice/Day-08B-Module"
+     ami = "ami-0f1dcc636b69a6438"
+    instance_type = "t2.medium"
+
+    
+}
--- Day-08-Module-Directory/main.tf
+++ Day-08-Module-Directory/main.tf
@@ -1,5 +1,5 @@
 module "dev" {
-    source = "../Day08-Module"
+    source = "../Day-08B-Module"
     ami = "ami-0f1dcc636b69a6438"
     instance_type = "t2.micro"
 
