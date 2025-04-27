434300de0cec288124b55eeb758b498e8fc8e09c
abhi <abhishek98@gmail.com>
2025-04-27 16:15:35 +0530

    add files for day 08

---
+++ Day-08-Module-Directory/main.tf
@@ -0,0 +1,10 @@
+module "dev" {
+    source = "../Day08-Module"
+    ami = "ami-0f1dcc636b69a6438"
+    instance_type = "t2.micro"
+
+    
+
+
+  
+}
---
+++ Day08-Module/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "name" {
+
+    ami = var.ami
+    instance_type = var.instance_type
+    subnet_id  = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+
+
+    tags = {
+      Name = "dev"
+    }
+  
+}
