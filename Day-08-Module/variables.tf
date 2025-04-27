05214eda376c18e8f649e63e152214cadfbe93f9
abhi <abhishek98@gmail.com>
2025-04-27 17:20:09 +0530

    a

--- Day-08-Module/variables.tf
+++ Day-08-Module/variables.tf
@@ -8,14 +8,4 @@ variable "instance_type" {
   
 }
 
-variable "subnet_id" {
-  description = "Subnet ID to launch the instance in"
-  type        = string
-  default = ""
-}
 
-variable "security_group_ids" {
-  description = "List of security group IDs"
-  type        = list(string)
-  default = [ "" ]
-}
